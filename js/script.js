3830708f91ba38ac3dc0bcb442218b6a33e9af2d
Rura Egor <ruraegor@gmail.com>
2018-05-31 00:27:40 +0300

    two

--- js/script.js
+++ js/script.js
@@ -7,12 +7,22 @@ $(".wr_img_sl img").each(function(){
 
 var trX = 220;
 
+var cc = 0;
+var deg_ret_all = 360 / ms_img_src.length;
+var deg_ret_item = [];
+
 for(var k = 0; k < ms_img_src.length; k++){
+
+    deg_ret_item.push(deg_ret_all * k);
+
 	var deg_ret = 360 / ms_img_src.length;
 	deg_ret = deg_ret * k;
-	//$("#cube").append('<div class="fig-'+k+' cn-img" style="background: url('+ms_img_src[k]+') no-repeat; -webkit-transform: rotateX(0deg) rotateY('+deg_ret+'deg) translateX(30px) translateY(0px) translateZ(0px);"><span>'+ k +'</span></div>');
 	$(".slider").append('<a href="'+ms_img_src[k]+'" rel="example_group" class="item fan" style="background-image: url('+ms_img_src[k]+'); transform: rotateY(0deg) translateX('+trX+'px)"></a>');
 }
+
+
+
+console.log( 'mmmmmmmmmmm', deg_ret_item );
 
 
 setTimeout("onSpiral()", 2000);
@@ -20,13 +30,9 @@ setTimeout("onSpiral()", 2000);
 //--------- FUNCTION -------------------------------
 
 function onSpiral() {
-	var cc = 0;
-	var deg_ret = 360 / ms_img_src.length;
 	
-	$(".slider .item").each(function(){
-		var deg_ret_item = deg_ret * cc;
-		$(this).css("transform", "rotateY(" + deg_ret_item + "deg) translateX(" + trX + "px)");
-		cc++;
+	$(".slider .item").each(function(i){
+		$(this).css("transform", "rotateY(" + deg_ret_item[i] + "deg) translateX(" + trX + "px)");
 	});
 	
 	setTimeout( ()=>{
@@ -41,7 +47,7 @@ function onSpiral() {
 
 
 var countSl = 0;
-var rotateSlX = -20;
+var rotateSlX = -17;
 var countSlDeg = 360 / ms_img_src.length;
 
 $('.slider').css("transform", "rotateX("+rotateSlX+"deg) rotateY(0deg)");
